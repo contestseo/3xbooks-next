fbbe7bf202191a3b739b17d8c5314c1e46e40bd2
contestseo <michael@contestseo.com>
2026-01-30 03:18:41 +0530

    redirect to author

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/authors",
+        destination: "/author",
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
